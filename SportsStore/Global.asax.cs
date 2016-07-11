675761455c71db9f9c805b205ae94817cb59c155
Dorin Balan <dorin.balan@amdaris.com>
2016-07-11 17:20:21 +0300

    first commit

---
+++ SportsStore/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using SportsStore.Domain.Entities;
+using SportsStore.WebUI;
+using SportsStore.WebUI.Infrastructure.Binders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SportsStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+			ModelBinders.Binders.Add(typeof(Cart), new AutoBinder<Cart>());
+
+        }
+    }
+}
---
+++ SportsStore/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+namespace SportsStore.WebUI.Infrastructure.Binders
+{
+	//public class CartModelBinder : IModelBinder
+	//{
+	//	private const string sessionKey = "Cart";
+	//	public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+	//	{
+	//		// get the Cart from the session
+	//		Cart cart = null;
+	//		if (controllerContext.HttpContext.Session != null)
+	//		{
+	//			cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+	//		}	
+	//		// create the Cart if there wasn't one in the session data
+	//		if (cart == null)
+	//		{
+	//			cart = new Cart();
+	//			if (controllerContext.HttpContext.Session != null)
+	//			{
+	//				controllerContext.HttpContext.Session[sessionKey] = cart;
+	//			}
+	//		}
+	//		// return the cart
+	//		return cart;
+	//	}
+	//}
+
+	public class AutoBinder<T> : IModelBinder where T : class
+	{
+		private readonly string sessionKey = nameof(T);
+		public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+		{
+			// get the Cart from the session
+			T cart = null;
+			if (controllerContext.HttpContext.Session != null)
+			{
+				cart = (T)controllerContext.HttpContext.Session[sessionKey];
+			}
+			// create the Cart if there wasn't one in the session data
+			if (cart == null)
+			{
+				cart = Activator.CreateInstance<T>();
+				if (controllerContext.HttpContext.Session != null)
+				{
+					controllerContext.HttpContext.Session[sessionKey] = cart;
+				}
+			}
+			// return the cart
+			return cart;
+		}
+	}
+	//public class DelegateBinder<T> : IModelBinder
+	//{
+	//	private Func<ControllerContext, ModelBindingContext, T> _delegate;
+
+	//	public DelegateBinder(Func<ControllerContext, ModelBindingContext, T> func)
+	//	{
+	//		_delegate = func;
+	//	}
+
+	//	public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+	//	{
+	//		return _delegate(controllerContext, bindingContext);
+	//	}
+	//}
+} 
